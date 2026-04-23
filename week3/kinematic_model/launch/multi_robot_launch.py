2e90f9aaecf6401e6e8f9d28266164b49e1f4464
JUan Paulo Salgado <A01737223@tec.mx>
2026-04-23 16:18:48 -0600

    feat: add reusable multi-robot launch and parameterized frames

---
+++ week3/kinematic_model/launch/multi_robot_launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('kinematic_model')
+    robot_group_launch = os.path.join(package_dir, 'launch', 'robot_group_launch.py')
+
+    robot1 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(robot_group_launch),
+        launch_arguments={
+            'namespace': 'robot1',
+            'initial_x': '0.0',
+            'initial_y': '0.0',
+            'initial_theta': '0.0',
+            'trajectory_type': 'square',
+            'side_length': '0.8',
+            'start_x': '0.0',
+            'start_y': '0.0',
+        }.items(),
+    )
+
+    robot2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(robot_group_launch),
+        launch_arguments={
+            'namespace': 'robot2',
+            'initial_x': '1.5',
+            'initial_y': '0.5',
+            'initial_theta': '0.0',
+            'trajectory_type': 'triangle',
+            'side_length': '0.6',
+            'start_x': '0.2',
+            'start_y': '0.2',
+        }.items(),
+    )
+
+    return LaunchDescription([
+        robot1,
+        robot2,
+    ])
---
+++ week3/kinematic_model/launch/robot_group_launch.py
@@ -0,0 +1,133 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, PushRosNamespace
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('kinematic_model')
+    urdf_file = os.path.join(package_dir, 'urdf', 'puzzlebot.urdf')
+    params_file = os.path.join(package_dir, 'config', 'robot_params.yaml')
+
+    with open(urdf_file, 'r', encoding='utf-8') as infp:
+        robot_description = infp.read()
+
+    namespace = LaunchConfiguration('namespace')
+    initial_x = LaunchConfiguration('initial_x')
+    initial_y = LaunchConfiguration('initial_y')
+    initial_theta = LaunchConfiguration('initial_theta')
+
+    trajectory_type = LaunchConfiguration('trajectory_type')
+    side_length = LaunchConfiguration('side_length')
+    start_x = LaunchConfiguration('start_x')
+    start_y = LaunchConfiguration('start_y')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('namespace', default_value='robot1'),
+        DeclareLaunchArgument('initial_x', default_value='0.0'),
+        DeclareLaunchArgument('initial_y', default_value='0.0'),
+        DeclareLaunchArgument('initial_theta', default_value='0.0'),
+        DeclareLaunchArgument('trajectory_type', default_value='square'),
+        DeclareLaunchArgument('side_length', default_value='0.8'),
+        DeclareLaunchArgument('start_x', default_value='0.0'),
+        DeclareLaunchArgument('start_y', default_value='0.0'),
+
+        GroupAction([
+            PushRosNamespace(namespace),
+
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='world_to_odom_broadcaster',
+                arguments=[
+                    '--x', initial_x,
+                    '--y', initial_y,
+                    '--z', '0.0',
+                    '--yaw', initial_theta,
+                    '--pitch', '0.0',
+                    '--roll', '0.0',
+                    '--frame-id', 'world',
+                    '--child-frame-id', [namespace, '/odom'],
+                ],
+                output='screen',
+            ),
+
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[
+                    params_file,
+                    {'robot_description': robot_description},
+                    {'frame_prefix': [namespace, '/']},
+                ],
+            ),
+
+            Node(
+                package='kinematic_model',
+                executable='kinematic_simulator',
+                name='puzzlebot_sim',
+                output='screen',
+                parameters=[
+                    params_file,
+                    {'publish_tf': False},
+                    {'publish_joint_states': False},
+                    {'x0': 0.0},
+                    {'y0': 0.0},
+                    {'theta0': 0.0},
+                    {'odom_frame': [namespace, '/odom']},
+                    {'base_frame': [namespace, '/base_footprint']},
+                    {'pose_frame': [namespace, '/odom']},
+                ],
+            ),
+
+            Node(
+                package='kinematic_model',
+                executable='dead_reckoning_localization',
+                name='dead_reckoning_localization',
+                output='screen',
+                parameters=[
+                    params_file,
+                    {'x0': 0.0},
+                    {'y0': 0.0},
+                    {'theta0': 0.0},
+                    {'odom_frame': [namespace, '/odom']},
+                    {'base_frame': [namespace, '/base_footprint']},
+                ],
+            ),
+
+            Node(
+                package='kinematic_model',
+                executable='point_stabilizer',
+                name='point_stabilizer',
+                output='screen',
+                parameters=[params_file],
+            ),
+
+            Node(
+                package='kinematic_model',
+                executable='setpoint_generator',
+                name='setpoint_generator',
+                output='screen',
+                parameters=[
+                    params_file,
+                    {'trajectory_type': trajectory_type},
+                    {'side_length': side_length},
+                    {'start_x': start_x},
+                    {'start_y': start_y},
+                ],
+            ),
+
+            Node(
+                package='kinematic_model',
+                executable='joint_state_publisher',
+                name='joint_state_publisher',
+                output='screen',
+                parameters=[params_file],
+            ),
+        ])
+    ])
