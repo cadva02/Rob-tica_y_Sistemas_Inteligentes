426bd5057b5981f74f39bd503609685d5d63d45f
cadva02 <a01735818@tec.mx>
2026-05-11 12:55:20 -0600

    real-robot

--- week5/puzzlebot_exp/launch/covariance_launch.py
+++ week5/puzzlebot_exp/launch/covariance_launch.py
@@ -27,6 +27,10 @@ def generate_launch_description():
     initial_x = LaunchConfiguration('initial_x')
     initial_y = LaunchConfiguration('initial_y')
     initial_theta = LaunchConfiguration('initial_theta')
+    real_namespace = LaunchConfiguration('real_namespace')
+    initial_x_real = LaunchConfiguration('initial_x_real')
+    initial_y_real = LaunchConfiguration('initial_y_real')
+    initial_theta_real = LaunchConfiguration('initial_theta_real')
 
     robot1_group = GroupAction([
         PushRosNamespace(namespace),
@@ -120,6 +124,98 @@ def generate_launch_description():
         ),
     ])
 
+    real_robot_group = GroupAction([
+        PushRosNamespace(real_namespace),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='world_to_odom_broadcaster_real',
+            arguments=[
+                '--x', initial_x_real,
+                '--y', initial_y_real,
+                '--z', '0.0',
+                '--yaw', initial_theta_real,
+                '--pitch', '0.0',
+                '--roll', '0.0',
+                '--frame-id', 'world',
+                '--child-frame-id', [real_namespace, '/odom'],
+            ],
+            output='screen',
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher_real',
+            output='screen',
+            parameters=[
+                params_file,
+                {'robot_description': robot_description},
+                {'frame_prefix': [real_namespace, '/']},
+            ],
+        ),
+
+        Node(
+            package='puzzlebot_exp',
+            executable='kinematic_simulator',
+            name='puzzlebot_sim_real',
+            output='screen',
+            parameters=[
+                params_file,
+                {'publish_tf': False},
+                {'publish_joint_states': False},
+                {'x0': 0.0},
+                {'y0': 0.0},
+                {'theta0': 0.0},
+                {'odom_frame': [real_namespace, '/odom']},
+                {'base_frame': [real_namespace, '/base_footprint']},
+                {'pose_frame': [real_namespace, '/odom']},
+            ],
+        ),
+
+        Node(
+            package='puzzlebot_exp',
+            executable='dead_reckoning_localization',
+            name='dead_reckoning_localization_real',
+            output='screen',
+            parameters=[
+                params_file,
+                {'x0': 0.0},
+                {'y0': 0.0},
+                {'theta0': 0.0},
+                {'sigma_v': 0.01},
+                {'sigma_w': 0.1},
+                {'odom_frame': [real_namespace, '/odom']},
+                {'base_frame': [real_namespace, '/base_footprint']},
+            ],
+        ),
+
+        Node(
+            package='puzzlebot_exp',
+            executable='point_stabilizer',
+            name='point_stabilizer_real',
+            output='screen',
+            parameters=[params_file],
+        ),
+
+        Node(
+            package='puzzlebot_exp',
+            executable='setpoint_generator',
+            name='setpoint_generator_real',
+            output='screen',
+            parameters=[params_file, setpoint_params],
+        ),
+
+        Node(
+            package='puzzlebot_exp',
+            executable='joint_state_publisher',
+            name='joint_state_publisher_real',
+            output='screen',
+            parameters=[params_file],
+        ),
+    ])
+
     rviz = Node(
         package='rviz2',
         executable='rviz2',
@@ -172,10 +268,15 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('namespace', default_value='robot1'),
+        DeclareLaunchArgument('real_namespace', default_value='robot1_real'),
         DeclareLaunchArgument('initial_x', default_value='0.0'),
         DeclareLaunchArgument('initial_y', default_value='0.0'),
         DeclareLaunchArgument('initial_theta', default_value='0.0'),
+        DeclareLaunchArgument('initial_x_real', default_value='0.0'),
+        DeclareLaunchArgument('initial_y_real', default_value='0.0'),
+        DeclareLaunchArgument('initial_theta_real', default_value='0.0'),
         robot1_group,
+        real_robot_group,
         rviz,
         rqt_tf_tree,
         rqt_graph,
